750842771cb20e69700bf61845c2450c9b456a7c
Barleyan <iyan.manisrejo@gmail.com>
2026-09-17 17:20:36 +0700

    Add sistem_fe project

---
+++ sistem_fe/app/page.js
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/hello`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  return (
+    <main>
+      <h1>Frontend Next.js</h1>
+      <p>{message}</p>
+    </main>
+  );
+}
